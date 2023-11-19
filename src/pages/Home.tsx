4ca3ee4d0687ac286f9ef824aeb45ee832d357a5
VaultDweller13 <grognak13@gmail.com>
2023-11-19 22:29:39 +0400

    feat: fetch client's ip from ip service API

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
 import { FeaturedCurrencies } from "../components/FeaturedCurrencies";
 import { Container } from "../layouts/Container";
 
+type LocationData = {
+  coutry_code: string;
+};
+
 export const Home = () => {
+  const endpoint = "http://api.ipstack.com/check?";
+  const access_key = import.meta.env.VITE_IP_API_KEY;
+  const [country, setCountry] = useState("");
+
+  useEffect(() => {
+    const fetchLocation = async () => {
+      const response = await fetch(
+        endpoint + new URLSearchParams({ access_key })
+      );
+
+      if (!response.ok) {
+        throw new Error("Request error");
+      }
+
+      const data = (await response.json()).country_code;
+      setCountry(data);
+    };
+
+    fetchLocation();
+  }, [access_key]);
+
   return (
     <Container>
       <h2>Home page</h2>
